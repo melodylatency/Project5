a0fb4222418655b4a5d24781e88b6c0f9b503501
melodylatency <melodylatency@yahoo.com>
2025-01-27 21:35:41 +0500

    random button

--- server.js
+++ server.js
@@ -99,6 +99,18 @@ app.get("/api/books", (req, res) => {
   res.json(books);
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running...");
+  });
+}
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
